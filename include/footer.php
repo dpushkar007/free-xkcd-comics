7a51cdf2dc6e65eb8dc61f9a3ddf1d1cbe3a2061
Pushkar Dounde <86927928+dpushkar007@users.noreply.github.com>
2021-08-24 22:23:19 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,37 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+			<title>FREE XKCD COMICS </title>
+			<link rel="shortcut icon" href="../media/favicon.ico">
+			<link rel="stylesheet" href="include/CSS/styles.css">
+    		<style type="text/css">
+				.footer
+				{
+					position: fixed;
+					padding: 1% 0 -2% 0 ;
+					margin: 2% 0 0 0;
+					background-color: #000;
+					left: 0px;
+					bottom: 0px;
+					text-align: center;
+					width: 100%;
+				}
+				p
+				{
+					color: white;
+				}
+			</style>
+		</head>
+		<body>
+			<table width="100%">
+				<footer class="footer">
+					<a style="color: azure; font-family: Tahoma, Verdana, sans-serif; font-size:large; color: azure;">"Excelsior"</a>
+				</footer>
+			</table>
+		</body>
+	</html>
---
+++ include/header.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+			<title>FREE XKCD COMICS </title>
+			<link rel="shortcut icon" href="../media/favicon1.ico">
+			<link rel="stylesheet" href="include/CSS/styles.css">
+			<style type="text/css">
+				hr
+				{
+					border:1px solid #D3D3D3;			
+				}
+				header
+				{
+					background-color: black;
+				}
+			</style>
+		</head>
+		<body>
+			<table width="100%">
+				<div class="header">
+					<tr>
+					<td><img src="https://xkcd.com/s/0b7742.png" alt="logo" vspace="10" hspace="10" height="40"></td>
+					</tr>
+				</div>
+			</table>
+			<hr>
+		</body>
+	</html>

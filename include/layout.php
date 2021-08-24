75692981dedea43d31e32623530a2947a6a864ba
Pushkar Dounde <86927928+dpushkar007@users.noreply.github.com>
2021-08-24 22:22:54 +0530

    Add files via upload

---
+++ include/layout.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+			<title>FREE XKCD COMICS </title>
+			<link rel="shortcut icon" href="../media/favicon1.ico">
+			<link rel="stylesheet" href="include/CSS/styles.css">
+		</head>
+		<body>
+			<header style="margin-top: -0.5%;">
+				<?php
+					require __DIR__.'/header.php';
+				?>
+			</header>
+			<body style="background-color: hsla(0, 0%, 0%, .8);">
+			</body>
+			<footer>
+				<?php
+					require __DIR__.'/footer.php';
+				?>
+			</footer>
+		</body>
+	</html>
